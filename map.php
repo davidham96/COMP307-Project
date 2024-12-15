4315e3ef12f570bc7ce2f0396652769bf3ae47bb
anna-jh <annajenkinshenderson@gmail.com>
2024-12-15 13:48:55 -0500

    Update map.php

--- map.php
+++ map.php
@@ -3,8 +3,7 @@ session_start();
 include './db.php';
 
 $conn = connectDatabase('map');
-//$user_id = $_SESSION['user_id'];
-$user_id =  100;
+$user_id = $_SESSION['user_id'];
 
 $stmt = $conn->prepare("SELECT id, longitude, latitude, place_name FROM locations WHERE user_id = ?");
 $stmt->bind_param("i", $user_id);

679a3e50c15d03dc3daffa50195e90b7df6b2164
anna-jh <155672700+anna-jh@users.noreply.github.com>
2024-12-15 13:40:57 -0500

    Add files via upload

---
+++ map.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include './db.php';
+
+$conn = connectDatabase('map');
+//$user_id = $_SESSION['user_id'];
+$user_id =  100;
+
+$stmt = $conn->prepare("SELECT id, longitude, latitude, place_name FROM locations WHERE user_id = ?");
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$locations = [];
+
+while ($row = $result->fetch_assoc()) {
+    $locations[] = $row;
+}
+
+echo json_encode($locations);
+
+$stmt->close();
+$conn->close();
+?>
